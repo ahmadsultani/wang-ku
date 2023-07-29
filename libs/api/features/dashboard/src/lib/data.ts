2da04fdd446dc5df48cabdb27ed5a8f2d9ec15e9
Ravi Deevan <ravideevan@duck.com>
2023-07-29 21:09:08 +0800

    fix: nullable values in dashboard

--- libs/api/features/dashboard/src/lib/data.ts
+++ libs/api/features/dashboard/src/lib/data.ts
@@ -16,11 +16,17 @@ export async function read(
         .selectAll()
         .executeTakeFirstOrThrow();
 
+      // __AUTO_GENERATED_PRINTF_START__
+      console.log('read#(anon) 1'); // __AUTO_GENERATED_PRINTF_END__
+
       const userVerification = await trx
         .selectFrom('users_verifications')
         .where('user_id', '=', user.id ?? '')
         .selectAll()
         .executeTakeFirstOrThrow();
+
+      // __AUTO_GENERATED_PRINTF_START__
+      console.log('read#(anon) 2'); // __AUTO_GENERATED_PRINTF_END__
 
       const { lended_total } = await trx
         .selectFrom('budget_requests')
@@ -29,20 +35,23 @@ export async function read(
         .select([sql<number>`sum(request_budget)`.as('lended_total')])
         .executeTakeFirstOrThrow();
 
-      const { status } = await trx
+      // __AUTO_GENERATED_PRINTF_START__
+      console.log('read#(anon) 3'); // __AUTO_GENERATED_PRINTF_END__
+
+      const lastRequest = await trx
         .selectFrom('budget_requests')
         .where('business_id', '=', business.id)
         .select('status')
         .orderBy('updated_at', 'desc')
         .limit(1)
-        .executeTakeFirstOrThrow();
+        .executeTakeFirst();
 
       return {
         lended_total,
         lend_limit: business.lend_limit ?? 0,
         monthly_spending: business.monthly_spending ?? 0,
         monthly_income: business.monthly_income ?? 0,
-        last_lend_status: status,
+        last_lend_status: lastRequest ? lastRequest.status : null,
         business: business ?? null,
         user_verification: userVerification ?? null,
       };
